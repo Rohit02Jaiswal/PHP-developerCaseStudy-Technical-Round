c700c6c9c0667b8bf1f97f16f5fd15082b0cea14
Rohit jaiswal <114230762+Rohit02Jaiswal@users.noreply.github.com>
2023-08-24 12:25:00 +0530

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,19 @@
+<?php
+$servername="localhost";
+$username="root";
+$password="";
+$dbname="saloon";
+$conn=mysqli_connect($servername,$username,$password,$dbname);
+if(!$conn){
+    die("Connection Failed: ". mysqli_connect_error());
+}
+elseif(!mysqli_select_db($conn, $dbname)){
+    die("Database Selection Failed: ". mysqli_error($conn));
+}
+else 
+
+{
+    // echo "Database Selected Successfully<br>";   
+
+}
+?>
